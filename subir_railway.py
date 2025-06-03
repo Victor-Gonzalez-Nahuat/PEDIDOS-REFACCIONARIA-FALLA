eee6de9f6114270a94402d28935d0bd8e574bda9
Victor Gonzalez Nahuat <A24216377@alumnos.uady.mx>
2025-06-02 22:26:21 -0600

    prueba 1

---
+++ subir_railway.py
@@ -0,0 +1,17 @@
+import subprocess
+import flet as ft
+
+def run_git_commands(commit_msg_output):
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", commit_msg_output])
+    subprocess.run(["git", "push"])
+
+def main(page: ft.Page):
+    page.title = "Subir a Railway"
+    
+    commit_msg = ft.TextField(label="Mensaje del commit", width=400)
+    button = ft.ElevatedButton("Subir a Railway", on_click=lambda e: run_git_commands(commit_msg.value))
+
+    page.add(commit_msg, button)
+
+ft.app(target=main)
