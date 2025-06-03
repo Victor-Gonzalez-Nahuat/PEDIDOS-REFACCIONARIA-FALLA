f3821e07b0800bd8fc3fc98b4861b031029634db
Victor Gonzalez Nahuat <A24216377@alumnos.uady.mx>
2025-06-02 22:27:10 -0600

    prueba 1

--- subir_railway.py
+++ subir_railway.py
@@ -4,7 +4,7 @@ import flet as ft
 def run_git_commands(commit_msg_output):
     subprocess.run(["git", "add", "."])
     subprocess.run(["git", "commit", "-m", commit_msg_output])
-    subprocess.run(["git", "push"])
+    subprocess.run(["git", "push", "origin", "master"])
 
 def main(page: ft.Page):
     page.title = "Subir a Railway"
